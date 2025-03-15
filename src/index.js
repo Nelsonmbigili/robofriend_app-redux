7fe488dfbefb18f0d441dd75420c729053ff87dd
Nelson Mbigili <107414047+Nelsonmbigili@users.noreply.github.com>
2025-03-15 22:23:48 +0100

    Initial commit to the new repository

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {createLogger} from 'redux-logger'
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import {searchRobots,requestRobots} from './reducers.js'
+import { thunk } from 'redux-thunk'
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots,requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunk,logger))
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+root.render(
+	<Provider store = {store}>
+		<App />
+	</Provider>);
+registerServiceWorker();
